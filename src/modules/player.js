154c2e2f6dfa7c2a30cc40b6f220d8a69a25cad4
Pete Fowler <fowler.pete@gmail.com>
2022-08-17 16:16:43 -0600

    Add boardOfAttack to player

--- src/modules/player.js
+++ src/modules/player.js
@@ -1,13 +1,14 @@
-const player = (moniker, typeOfPlayer) => {
+const player = (moniker, board, typeOfPlayer) => {
   const name = moniker;
   const type = typeOfPlayer;
+  let boardOfAttack = board;
   const isCurrentTurn = false;
   const getTurn = () => isCurrentTurn;
   const attack = (board, x, y, axis) => {
     board.incoming(x, y, axis)
   };
 
-  return { name, type, isCurrentTurn, getTurn, attack }
+  return { name, type, boardOfAttack, isCurrentTurn, getTurn, attack }
 }
 
 export default player;
--- tests/player.test.js
+++ tests/player.test.js
@@ -4,17 +4,18 @@ import gameBoard from "../src/modules/gameBoard";
 import makeShip from "../src/modules/ship";
 
 describe('player', () => {
-  beforeEach(() => {
-    const p1 = player('Sal', 'human');
+  // beforeEach(() => {
     const board = gameBoard();
     board.init();
+    const p1 = player('Sal', board, 'human');
     const sub = makeShip('sub');
     board.place(sub, 0, 0, 'x');
-  });
+  // });
   test('player object created', () => {
     expect(p1.name).toBe('Sal');
     expect(p1.type).toBe('human');
     expect(!!p1.attack).toEqual(true);
+    expect(p1.boardOfAttack).toBe(board);
   });
   test('attack', () => {
     p1.attack(0, 2);
