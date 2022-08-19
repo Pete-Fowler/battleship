52f1d100bd8641f5a4196d4445497fff22837197
Pete Fowler <fowler.pete@gmail.com>
2022-08-19 16:14:06 -0600

    Write test: AI fires shot

--- tests/player.test.js
+++ tests/player.test.js
@@ -4,28 +4,40 @@ import gameBoard from "../src/modules/gameBoard";
 import makeShip from "../src/modules/ship";
 
 describe('player', () => {
+  const boardTemplate = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]];
   const board = gameBoard();
   board.init();
-  const p1 = player('Sal', board, 'human');
+  
+  const AI = player('Gustov', board, 'AI')
+  const P1 = player('Sal', board, 'human');
   const sub = makeShip('sub');
   board.place(sub, 0, 0, 'y');
   
+  beforeEach(() => {
+    board.init();
+  });
   test('player object created', () => {
-    expect(p1.name).toBe('Sal');
-    expect(p1.type).toBe('human');
-    expect(!!p1.attack).toEqual(true);
-    expect(p1.boardOfAttack).toBe(board);
+    expect(P1.name).toBe('Sal');
+    expect(P1.type).toBe('human');
+    expect(!!P1.attack).toEqual(true);
+    expect(P1.boardOfAttack).toBe(board);
   });
   test('player attack', () => {
-    p1.attack(0, 0);
+    P1.attack(0, 0);
     expect(sub.getHull()).toEqual([1, 0, 0]);
-    p1.attack(0, 2);
+    P1.attack(0, 2);
     expect(sub.getHull()).toEqual([1, 0, 1]);
-    p1.attack(0, 1);
+    P1.attack(0, 1);
     expect(sub.getHull()).toEqual([1, 1, 1]);
     expect(sub.isSunk()).toBe(true);
   });
-  test('AI Attack', () => {
-    
+  test('AI fires a shot', () => {
+    console.log(board.getMap);
+    AI.attack();
+    expect(board.getMap()).not.toEqual(boardTemplate);
+    console.log(board.getMap);
   })
 });
