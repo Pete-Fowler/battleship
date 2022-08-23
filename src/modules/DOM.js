431e304b7c24aa0a5bb50eeaf6c1313f40e21dfc
Pete Fowler <fowler.pete@gmail.com>
2022-08-23 14:40:52 -0600

    Change AI in game loop to p2

--- src/modules/DOM.js
+++ src/modules/DOM.js
@@ -15,7 +15,22 @@ import {
   AICarrier
 } from './game';
 
+const p1Box = document.querySelector('#p1');
+const p2Box = document.querySelector('#p2');
+
 const renderBoard = (board, player) => {
+  // Outer board container
+  const boardBox = document.createElement('div');
+  boardBox.id = `${player.name}-board`;
+
+  // Individual squares on board
+  for(let i = 0; i <= 9; i += 1) {
+    for(let j = 0; j <= 9; j += 1) {
+      const square = document.createElement('div');
+      square.textContent = board.getMap[i][j];
+      boardBox.append(square);
+    }
+  }
   
 }
 
@@ -29,3 +44,5 @@ const renderBoard = (board, player) => {
 // from other objects. If at any point you are tempted to write a new function 
 // inside the game loop, step back and figure out which class or module that 
 // function should belong to.
+
+export { renderBoard };
