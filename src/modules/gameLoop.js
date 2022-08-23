c268c6572b1ed72e99560a94b75021deec641cca
Pete Fowler <fowler.pete@gmail.com>
2022-08-23 13:41:25 -0600

    Draft gameLoop
    Ships manually placed. Will need to implement user drag and drop input.

--- src/modules/gameLoop.js
+++ src/modules/gameLoop.js
@@ -1,12 +1,41 @@
 import gameBoard from "./gameBoard";
 import player from "./player";
+import makeShip from './ship';
 
-const gameLoop = (() => {
-  const playerBoard = gameBoard();
-  const AIboard = gameBoard();
-  const p1 = player("Gustav", AIboard, "human");
-  const AI = player("Terminator", playerBoard, "computer");
-})();
+// Make game boards
+const playerBoard = gameBoard();
+const AIBoard = gameBoard();
+
+// Make players
+const p1 = player('Gustav', AIboard, "human");
+const AI = player("Terminator", playerBoard, "computer");
+
+// Make p1 ships
+const p1Ptb = makeShip('patrolBoat');
+const p1Sub = makeShip('sub');
+const p1Destroyer = makeShip('destroyer');
+const p1Battleship = makeShip('battleship');
+const p1Carrier = makeShip('carrier');
+
+// Make AI ships
+const AIPtb = makeShip('patrolBoat');
+const AISub = makeShip('sub');
+const AIDestroyer = makeShip('destroyer');
+const AIBattleship = makeShip('battleship');
+const AICarrier = makeShip('carrier');
+
+// Place ships                            Will need editing for player control
+playerBoard.place(p1Ptb, 2, 0, 'x');
+playerBoard.place(p1Sub, 7, 2, 'x');
+playerBoard.place(p1Destroyer, 2, 4, 'x');
+playerBoard.place(p1Battleship, 6, 6, 'x');
+playerBoard.place(p1Carrier, 4, 8, 'x');
+
+AIBoard.place(AIPtb, 0, 1, 'y');
+AIBoard.place(AISub, 2, 6, 'y');
+AIBoard.place(AIDestroyer, 4, 2, 'y');
+AIBoard.place(AIBattleship, 6, 6, 'y');
+AIBoard.place(AICarrier, 8, 4, 'y');
 
 // The game loop should set up a new game by creating Players and Gameboards.
 // For now just populate each Gameboard with predetermined coordinates. You can
